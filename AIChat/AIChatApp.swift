80e9c58c84b4d66753bc51add8c2a404432f9445
JackaDu <jackdu.academic@gmail.com>
2025-08-31 00:22:06 +0800

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Hao Du on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIChat
+//
+//  Created by Hao Du on 8/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
